814a799fdd153fd329ede22bf44c929a6d8e04e5
Johannes Kruisselbrink <johannes.kruisselbrink@wur.nl>
2023-05-16 07:54:03 +0200

    fix: update exposure matrix builder unit test to expect exception when there are no (positive) exposures

--- MCRA.Simulation.Test/UnitTests/Calculators/ComponentCalculation/ExposureMatrixCalculation/ExposureMatrixBuilderTests.cs
+++ MCRA.Simulation.Test/UnitTests/Calculators/ComponentCalculation/ExposureMatrixCalculation/ExposureMatrixBuilderTests.cs
@@ -13,7 +13,6 @@ namespace MCRA.Simulation.Test.UnitTests.Calculators.MixtureCalculation {
     [TestClass]
     public class ExposureMatrixBuilderTests {
 
-
         /// <summary>
         /// Test exposure based acute data matrix
         /// </summary>
@@ -54,6 +53,7 @@ namespace MCRA.Simulation.Test.UnitTests.Calculators.MixtureCalculation {
             Assert.AreEqual(substances.Count, result.Exposures.RowDimension);
             Assert.AreEqual(positivesCount, result.Exposures.ColumnDimension);
         }
+
         /// <summary>
         /// Test risk based acute data matrix
         /// </summary>
@@ -99,6 +99,7 @@ namespace MCRA.Simulation.Test.UnitTests.Calculators.MixtureCalculation {
         /// Test empty data matrix chronic
         /// </summary>
         [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
         public void ExposureMatrixBuilder_TestChronicEmpty() {
             var substances = MockSubstancesGenerator.Create(4);
             var rpfs = substances.ToDictionary(r => r, r => 1D);
@@ -108,15 +109,13 @@ namespace MCRA.Simulation.Test.UnitTests.Calculators.MixtureCalculation {
                 substances: substances,
                 relativePotencyFactors: rpfs,
                 membershipProbabilities: memberships,
-                exposureType: ExposureType.Acute,
+                exposureType: ExposureType.Chronic,
                 isPerPerson: false,
                 exposureApproachType: ExposureApproachType.RiskBased,
                 totalExposureCutOff: 0,
                 ratioCutOff: 0
             );
             var result = builder.Compute(null, individualDayExposures);
-            Assert.AreEqual(0, result?.Exposures?.RowDimension ?? 0);
-            Assert.AreEqual(0, result?.Exposures?.ColumnDimension ?? 0);
         }
     }
 }
