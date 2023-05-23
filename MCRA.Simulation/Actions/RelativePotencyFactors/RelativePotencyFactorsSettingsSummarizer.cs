dc404eb2333c52131d542417a0deabf38986b75a
Johannes Kruisselbrink <johannes.kruisselbrink@wur.nl>
2023-05-23 09:50:45 +0200

    fix: move summarizing of reference substance from substances action to RPFs action

--- MCRA.Simulation/Actions/RelativePotencyFactors/RelativePotencyFactorsSettingsSummarizer.cs
+++ MCRA.Simulation/Actions/RelativePotencyFactors/RelativePotencyFactorsSettingsSummarizer.cs
@@ -2,6 +2,7 @@
 using MCRA.General;
 using MCRA.General.Action.Settings.Dto;
 using MCRA.Simulation.Action;
+using MCRA.General.SettingsDefinitions;
 
 namespace MCRA.Simulation.Actions.RelativePotencyFactors {
     public class RelativePotencyFactorsSettingsSummarizer : ActionSettingsSummarizerBase {
@@ -10,6 +11,8 @@ namespace MCRA.Simulation.Actions.RelativePotencyFactors {
         public override ActionSettingsSummary Summarize(ProjectDto project) {
             var section = new ActionSettingsSummary(ActionType.GetDisplayName());
             summarizeDataOrCompute(project, section);
+            var es = project.EffectSettings;
+            section.SummarizeSetting(SettingsItemType.CodeReferenceCompound, es.CodeReferenceCompound, !string.IsNullOrEmpty(es.CodeReferenceCompound));
             return section;
         }
     }
--- MCRA.Simulation/Actions/Substances/SubstancesSettingsSummarizer.cs
+++ MCRA.Simulation/Actions/Substances/SubstancesSettingsSummarizer.cs
@@ -13,15 +13,8 @@ namespace MCRA.Simulation.Actions.Substances {
         public override ActionSettingsSummary Summarize(ProjectDto project) {
             var section = new ActionSettingsSummary(ActionType.GetDisplayName());
             summarizeDataSources(project, section);
-            if (project.AssessmentSettings.MultipleSubstances && project.AssessmentSettings.Cumulative) {
-                var es = project.EffectSettings;
-                section.SummarizeSetting(SettingsItemType.CodeReferenceCompound, es.CodeReferenceCompound, !string.IsNullOrEmpty(es.CodeReferenceCompound));
-            }
             if (project.AssessmentSettings.MultipleSubstances) {
                 section.SummarizeSetting(SettingsItemType.MultipleSubstances, project.AssessmentSettings.MultipleSubstances);
-            }
-            if (!project.AssessmentSettings.Cumulative) {
-                section.SummarizeSetting(SettingsItemType.Cumulative, project.AssessmentSettings.Cumulative, isVisible: false);
             }
             return section;
         }
