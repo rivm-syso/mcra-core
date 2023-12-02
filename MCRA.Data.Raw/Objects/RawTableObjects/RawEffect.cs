72e6253c3cb11c01e4343baa00591cf19b0acc61
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2023-12-02 15:56:56 +0100

    feat: add raw table object for effects

---
+++ MCRA.Data.Raw/Objects/RawTableObjects/RawEffect.cs
@@ -0,0 +1,24 @@
+﻿using MCRA.General;
+
+namespace MCRA.Data.Raw.Objects.RawTableObjects {
+    [RawDataSourceTableID(RawDataSourceTableID.Effects)]
+    public class RawEffect : IRawDataTableRecord {
+        public string idEffect { get; set; }
+        public string CodeSystem { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string BiologicalOrganisation { get; set; }
+        public string KeyEventProcess { get; set; }
+        public string KeyEventObject { get; set; }
+        public string KeyEventAction { get; set; }
+        public string KeyEventOrgan { get; set; }
+        public string KeyEventCell { get; set; }
+        public string RiskType { get; set; }
+        public string AOPWikiIds { get; set; }
+        public string Reference { get; set; }
+        public bool? IsDevelopmental { get; set; }
+        public bool? IsGenotoxic { get; set; }
+        public bool? IsAChEInhibitor { get; set; }
+        public bool? IsNonGenotoxicCarcinogenic { get; set; }
+    }
+}
