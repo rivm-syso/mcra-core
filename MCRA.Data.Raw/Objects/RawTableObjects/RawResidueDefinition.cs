607071d04c4527707fc7733379edc2893dcab67b
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2023-12-02 14:45:16 +0100

    feat: add raw residue definition data record object

---
+++ MCRA.Data.Raw/Objects/RawTableObjects/RawResidueDefinition.cs
@@ -0,0 +1,13 @@
+﻿using MCRA.General;
+
+namespace MCRA.Data.Raw.Objects.RawTableObjects {
+
+    [RawDataSourceTableID(RawDataSourceTableID.ResidueDefinitions)]
+    public sealed class RawResidueDefinition : IRawDataTableRecord {
+        public string idMeasuredSubstance { get; set; }
+        public string idActiveSubstance { get; set; }
+        public double ConversionFactor { get; set; }
+        public bool IsExclusive { get; set; }
+        public double? Proportion { get; set; }
+    }
+}
