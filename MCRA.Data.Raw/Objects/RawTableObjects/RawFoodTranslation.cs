f740a8b7bbd87ac33001697844caf475b035d81a
Johannes Kruisselbrink <johannes.kruisselbrink@wur.nl>
2023-12-04 12:29:04 +0100

    feat: add raw table object for food translations

---
+++ MCRA.Data.Raw/Objects/RawTableObjects/RawFoodTranslation.cs
@@ -0,0 +1,11 @@
+﻿using MCRA.General;
+
+namespace MCRA.Data.Raw.Objects.RawTableObjects {
+    [RawDataSourceTableID(RawDataSourceTableID.FoodTranslations)]
+    public class RawFoodTranslation : IRawDataTableRecord {
+        public string idFromFood { get; set; }
+        public string idToFood { get; set; }
+        public double Proportion { get; set; }
+        public string idPopulation { get; set; }
+    }
+}
