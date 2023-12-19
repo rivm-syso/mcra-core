831242e60ebce77d28c9ee680e7ee52bfb405081
Johannes Kruisselbrink <johannes.kruisselbrink@wur.nl>
2023-12-19 08:33:54 +0100

    feat: add raw table object for food unit weights

---
+++ MCRA.Data.Raw/Objects/RawTableObjects/RawFoodUnitWeight.cs
@@ -0,0 +1,13 @@
+﻿using MCRA.General;
+
+namespace MCRA.Data.Raw.Objects.RawTableObjects {
+    [RawDataSourceTableID(RawDataSourceTableID.FoodUnitWeights)]
+    public class RawFoodUnitWeight : IRawDataTableRecord {
+        public string idFood { get; set; }
+        public string Location { get; set; }
+        public UnitWeightValueType ValueType { get; set; }
+        public ValueQualifier Qualifier { get; set; }
+        public double Value { get; set; }
+        public string Reference { get; set; }
+    }
+}
