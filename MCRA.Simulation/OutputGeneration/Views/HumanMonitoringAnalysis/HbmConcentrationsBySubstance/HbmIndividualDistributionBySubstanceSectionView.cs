2ee881c360dfbdaaae973ec17ae2c744dad0e57d
Johannes Kruisselbrink <johannes.kruisselbrink@wur.nl>
2024-01-25 12:59:55 +0100

    fix: Update HBM individual distributions by substance table to hide column exposure route when not applicable

--- MCRA.Simulation/OutputGeneration/Views/HumanMonitoringAnalysis/HbmConcentrationsBySubstance/HbmIndividualDistributionBySubstanceSectionView.cs
+++ MCRA.Simulation/OutputGeneration/Views/HumanMonitoringAnalysis/HbmConcentrationsBySubstance/HbmIndividualDistributionBySubstanceSectionView.cs
@@ -11,6 +11,9 @@ namespace MCRA.Simulation.OutputGeneration.Views {
             if (Model.IndividualRecords.All(r => string.IsNullOrEmpty(r.BiologicalMatrix))) {
                 hiddenProperties.Add("BiologicalMatrix");
             }
+            if (Model.IndividualDayRecords.All(r => string.IsNullOrEmpty(r.ExposureRoute))) {
+                hiddenProperties.Add("ExposureRoute");
+            }
             if (Model.IndividualRecords.All(r => double.IsNaN(r.MedianAllLowerBoundPercentile))) {
                 hiddenProperties.Add("MedianAllMedianPercentile");
                 hiddenProperties.Add("MedianAllLowerBoundPercentile");
