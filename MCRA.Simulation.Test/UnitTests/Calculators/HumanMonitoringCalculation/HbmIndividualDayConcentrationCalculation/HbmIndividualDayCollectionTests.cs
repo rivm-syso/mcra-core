fc9779f56af36749d8a0c881ed115e443e2d08c2
Voorthuijsen, Tijmen van <tijmen.vanvoorthuijsen@wur.nl>
2024-01-30 13:36:27 +0100

    test: unit test for cloning hbm individual day collection and concentration

---
+++ MCRA.Simulation.Test/UnitTests/Calculators/HumanMonitoringCalculation/HbmIndividualDayConcentrationCalculation/HbmIndividualDayCollectionTests.cs
@@ -0,0 +1,40 @@
+﻿using MCRA.General;
+using MCRA.Simulation.Test.Mock.MockDataGenerators;
+using MCRA.Utils.Statistics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCRA.Simulation.Test.UnitTests.Calculators.HumanMonitoringCalculation.HbmIndividualDayConcentrationCalculation {
+    [TestClass]
+    public class HbmIndividualDayCollectionTests {
+
+        [TestMethod]
+        public void HbmIndividualDayCollection_Clone_ShouldReturnNewObjectInstance() {
+            // Arrange
+            var seed = 1;
+            var random = new McraRandomGenerator(seed);
+            var substances = MockSubstancesGenerator.Create(3);
+            var individuals = MockIndividualsGenerator.Create(1, 1, random, useSamplingWeights: true);
+            var individualDays = MockIndividualDaysGenerator.CreateSimulatedIndividualDays(individuals);
+            var samplingMethod = FakeHbmDataGenerator.FakeHumanMonitoringSamplingMethod();
+            var targetUnit = TargetUnit.FromInternalDoseUnit(DoseUnit.ugPerL, BiologicalMatrix.Urine, ExpressionType.Creatinine);
+
+            var originalndividualDayCollection = FakeHbmIndividualDayConcentrationsGenerator.Create(individualDays, substances, samplingMethod, targetUnit, random).First();
+            var originalIndividualDayConcentration = originalndividualDayCollection.HbmIndividualDayConcentrations.First();
+            var originalIndividualDay = "Original";
+            originalIndividualDayConcentration.Day = originalIndividualDay;
+
+            // Act
+            var clonedIndividualDayCollection = originalndividualDayCollection.Clone();
+            var clonedIndividualDayConcentration = clonedIndividualDayCollection.HbmIndividualDayConcentrations.First();
+            var clonedIndividualDay = "Clone";
+            clonedIndividualDayConcentration.Day = clonedIndividualDay;
+
+            // Assert
+            Assert.IsFalse(ReferenceEquals(clonedIndividualDayCollection, originalndividualDayCollection));
+            Assert.IsFalse(ReferenceEquals(clonedIndividualDayConcentration, originalndividualDayCollection));
+            Assert.AreEqual(originalIndividualDay, originalIndividualDayConcentration.Day);
+            Assert.AreEqual(clonedIndividualDay, clonedIndividualDayConcentration.Day);
+            Assert.AreNotEqual(originalIndividualDayConcentration.Day, clonedIndividualDayConcentration.Day);
+        }
+    }
+}
