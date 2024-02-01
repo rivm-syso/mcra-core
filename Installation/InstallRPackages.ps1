236bbe996baff4680c8db249e60646a9aa507e95
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2024-02-01 11:52:27 +0100

    refactor: install R packages from github

--- Installation/InstallRPackages.ps1
+++ Installation/InstallRPackages.ps1
@@ -1,39 +1,23 @@
 # Description   : this script installs additional R packages required for running MCRA Core.
 # Creation date : 09-Dec-2022
+# Modify date   : 01-Feb-2023
 
 # Definitions
 $RVersion="4.2.1"
+$RVersionMajorMinor="4.2"
+$RVersionMajorMinorProast="4.0"
 $Command = "$Env:Programfiles\R\R-$RVersion\bin\rscript.exe"
-$LibTargetFolder=("$Env:Programfiles\R\R-$RVersion\library").Replace('\', '\\')
 $PackageListFilePath = ".\rpackages.txt"
-$ProastUrl = "https://www.rivm.nl/sites/default/files/2021-06/proast70.3.zip"
 
-# Creates a unique temp folder to store the downloaded zip file
-Function New-TemporaryFolder {
-    # Make a new folder based upon a TempFileName
-    $T="$($Env:temp)\tmp$([convert]::tostring((get-random 65535),16).padleft(4,'0')).tmp"
-    New-Item -ItemType Directory -Path $T
-}
-$TempFolder = New-TemporaryFolder
-
-# Exception: install the PROAST R package from a downloadeded zip file
-Function InstallProastPackage {
-    $DownloadZipFile = "$TempFolder\" + $(Split-Path -Path $ProastUrl -Leaf)
-    Invoke-WebRequest -Uri $ProastUrl -OutFile $DownloadZipFile
-
-    $DownloadZipFile = $DownloadZipFile.Replace('\', '\\')
-    & "$Command" -e "install.packages('$DownloadZipFile', '$LibTargetFolder', repos=NULL, type='binary')"
-}
-
-# Install all packeges defined in 
+# Install all packages defined in 
 $PackagesList = Get-Content -Path $PackageListFilePath
 ForEach ($Package in $PackagesList)
 {
-    $Package = $Package.Trim()
-    if ($Package.Contains("proast")) { 
-        InstallProastPackage
-      } 
-      else {
-        & "$Command" -e "install.packages('$Package', '$LibTargetFolder', repos='https://cloud.r-project.org', type='binary')"
-      }
-}
+	$Package = $Package.Trim()
+	if ($Package.Contains("proast")) { 
+	    & "$Command" -e "install.packages('$Package', contriburl='https://biometris.github.io/MCRARpackages/bin/windows/contrib/$RVersionMajorMinorProast', type='win.binary')"	
+    } 
+	else {
+		& "$Command" -e "install.packages('$Package', contriburl='https://biometris.github.io/MCRARpackages/bin/windows/contrib/$RVersionMajorMinor', type='win.binary')"	
+	}
+ }
