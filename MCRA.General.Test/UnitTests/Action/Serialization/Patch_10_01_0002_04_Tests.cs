332c7cb78ea57558cda6f404d46af8c3dd45b2d3
Johannes Kruisselbrink <johannes.kruisselbrink@wur.nl>
2024-11-01 11:13:30 +0100

    feat: add settings patch for renamed selection value UseConsumptions of DustExposuresIndividualGenerationMethod

---
+++ MCRA.General.Test/UnitTests/Action/Serialization/Patch_10_01_0002_04_Tests.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml;
+using MCRA.General.Action.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModuleSettingsType = (string moduleId, (string key, string value)[])[];
+
+namespace MCRA.General.Test.UnitTests.Action.Serialization {
+    [TestClass]
+    public class Patch_10_01_0002_04_Tests : ProjectSettingsSerializerTestsBase {
+
+        [TestMethod]
+        [DataRow("UseConsumptions", DustExposuresIndividualGenerationMethod.UseDietaryExposures)]
+        [DataRow("Simulate", DustExposuresIndividualGenerationMethod.Simulate)]
+        public void Patch_10_01_0002_04_Test(
+            string oldGenerationMethod,
+            DustExposuresIndividualGenerationMethod expectedGenerationMethod
+        ) {
+            ModuleSettingsType moduleSettings = [
+                ("DustExposures",
+                    [
+                        ("NumberOfSimulatedIndividuals", $"{111}"),
+                        ("DustExposuresIndividualGenerationMethod", oldGenerationMethod.ToString())
+                    ]
+                )
+            ];
+            var xmlOld = createMockSettingsXml(moduleSettings, new(10, 1, 1));
+            var newXml = ProjectSettingsSerializer.GetTransformedSettingsXml(xmlOld);
+            var settingsDto = ProjectSettingsSerializer.ImportFromXmlString(xmlOld, null, false, out _);
+            Assert.AreEqual(expectedGenerationMethod, settingsDto.DustExposuresSettings.DustExposuresIndividualGenerationMethod);
+            Assert.AreEqual(111, settingsDto.DustExposuresSettings.NumberOfSimulatedIndividuals);
+        }
+
+        [TestMethod]
+        public void Patch_10_01_0002_04_Test_NoSettings() {
+            ModuleSettingsType moduleSettings = [
+                ("TargetExposures", [])
+            ];
+            var xmlOld = createMockSettingsXml(moduleSettings, new(10, 1, 1));
+            var newXml = ProjectSettingsSerializer.GetTransformedSettingsXml(xmlOld);
+            var settingsDto = ProjectSettingsSerializer.ImportFromXmlString(xmlOld, null, false, out _);
+            Assert.AreEqual(DustExposuresIndividualGenerationMethod.Simulate, settingsDto.DustExposuresSettings.DustExposuresIndividualGenerationMethod);
+        }
+    }
+}
---
+++ MCRA.General/Action/Serialization/Transforms/Patch-10.01.0002-04.xslt
@@ -0,0 +1,28 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!--
+Stylesheet for transforms from version 10.1.1 to version 10.1.2:
+Change UseConsumptions to UseDietaryExposures for DustExposuresIndividualGenerationMethod value.
+-->
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl">
+  <xsl:output method="xml" indent="yes" />
+
+  <!-- Copy all nodes and attributes, applying the templates hereafter -->
+  <xsl:template match="@*|node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@*|node()" />
+    </xsl:copy>
+  </xsl:template>
+
+  <!--Rename Consumptions to DietaryExposures for DustExposuresIndividualGenerationMethod value -->
+  <xsl:template match="ModuleConfigurations/ModuleConfiguration[@module='DustExposures']/Settings/Setting[@id='DustExposuresIndividualGenerationMethod']">
+    <Setting id="DustExposuresIndividualGenerationMethod">
+      <xsl:choose>
+        <xsl:when test=". = 'UseConsumptions'">UseDietaryExposures</xsl:when>
+        <xsl:otherwise>
+          <xsl:value-of select="text()" />
+        </xsl:otherwise>
+      </xsl:choose>
+    </Setting>
+  </xsl:template>
+</xsl:stylesheet>
