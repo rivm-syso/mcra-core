0fe468a439c0d4b0c14c32854cb5edff786bafc9
Lenthe, Marco van <marco.vanlenthe@wur.nl>
2025-02-05 15:31:07 +0100

    fix: Focal Concentrations sample subset selection is not summarized

--- MCRA.Simulation/Actions/FocalFoodConcentrations/FocalFoodConcentrationsSettingsSummarizer.cs
+++ MCRA.Simulation/Actions/FocalFoodConcentrations/FocalFoodConcentrationsSettingsSummarizer.cs
@@ -16,6 +16,21 @@ namespace MCRA.Simulation.Actions.FocalFoodConcentrations {
             var section = new ActionSettingsSummary(ActionType.GetDisplayName());
             section.SummarizeSetting(SettingsItemType.FocalFoods, string.Join(", ", _configuration.FocalFoods.Select(r => r.CodeFood)));
             section.SummarizeSetting(SettingsItemType.FocalSubstances, string.Join(", ", _configuration.FocalFoods.Select(r => r.CodeSubstance)));
+
+            section.SummarizeSetting(SettingsItemType.SampleSubsetSelection, _configuration.SampleSubsetSelection);
+            if (_configuration.SampleSubsetSelection) {
+                if (_configuration.SamplesSubsetDefinitions?.Count > 0) {
+                    foreach (var subset in _configuration.SamplesSubsetDefinitions) {
+                        if (subset.AlignSubsetWithPopulation) {
+                            section.SummarizeSetting($"Sample subset on {subset.PropertyName}", "Aligned with population");
+                        } else {
+                            section.SummarizeSetting($"Sample subset on {subset.PropertyName}", string.Join(", ", subset.KeyWords.Distinct()));
+                        }
+                        section.SummarizeSetting($"Include records with unspecified {subset.PropertyName}", subset.IncludeMissingValueRecords);
+                    }
+                }
+            }
+
             return section;
         }
     }
