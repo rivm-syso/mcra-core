8b8dc8eec0cdc8a9742813318ed88cae080215f2
Waldo de Boer <waldo.deboer@wur.nl>
2025-03-31 15:52:36 +0200

    feature: replace data table with boxplots and summary table, generic classes

---
+++ MCRA.Simulation/OutputGeneration/Generic/ReportChartCreators/PercentilesRecord.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace MCRA.Simulation.OutputGeneration {
+    public sealed class PercentilesRecord : BoxPlotChartRecord {
+
+        [Description("Substance name")]
+        [DisplayName("Substance name")]
+        public string SubstanceName { get; set; }
+
+        [Description("Substance code")]
+        [DisplayName("Substance code")]
+        public string SubstanceCode { get; set; }
+
+        [Description("Number of measurements.")]
+        [DisplayName("Number of measurements")]
+        public int NumberOfMeasurements { get; set; }
+
+        public override string GetLabel() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MCRA.Simulation/OutputGeneration/Generic/ReportChartCreators/SubstanceConcentrationRecord.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MCRA.Simulation.OutputGeneration {
+
+    public sealed class SubstanceConcentrationRecord {
+        [Description("Substance name")]
+        [DisplayName("Substance name")]
+        public string SubstanceName { get; set; }
+
+        [Description("Substance code")]
+        [DisplayName("Substance code")]
+        public string SubstanceCode { get; set; }
+
+        [Description("The target unit of the concentration values.")]
+        [DisplayName("Unit")]
+        public string Unit { get; set; }
+
+        [Description("The total number of samples.")]
+        [DisplayName("Total samples (N)")]
+        public int SamplesTotal { get; set; }
+
+        [Description("The number of positive measurement values.")]
+        [DisplayName("Positives")]
+        public int PositiveMeasurements { get; set; }
+
+        [Description("The mean of the positive measurement values.")]
+        [DisplayName("Mean positive samples")]
+        [DisplayFormat(DataFormatString = "{0:G3}")]
+        public double MeanPositives { get; set; }
+
+        [Description("The median of the positive measurement values.")]
+        [DisplayName("Median positive samples")]
+        [DisplayFormat(DataFormatString = "{0:G3}")]
+        public double MedianPositives { get; set; }
+
+        [Description("The lower percentile point of the positive measurement values.")]
+        [DisplayName("Lower {LowerPercentage} positives")]
+        [DisplayFormat(DataFormatString = "{0:G3}")]
+        public double LowerPercentilePositives { get; set; }
+
+        [Description("The upper percentile point of the positive measurement values.")]
+        [DisplayName("Upper {UpperPercentage} positives")]
+        [DisplayFormat(DataFormatString = "{0:G3}")]
+        public double UpperPercentilePositives { get; set; }
+    }
+}
