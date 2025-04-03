06ce1a220fe9013d41272a077c096ec00a056ef0
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2025-04-03 11:15:09 +0200

    feat: link bodindicator data with populations (#2150)

--- MCRA.Simulation/Actions/BaselineBodIndicators/BaselineBodIndicatorsActionCalculator.cs
+++ MCRA.Simulation/Actions/BaselineBodIndicators/BaselineBodIndicatorsActionCalculator.cs
@@ -18,6 +18,7 @@ namespace MCRA.Simulation.Actions.BaselineBodIndicators {
 
         protected override void verify() {
             _actionDataLinkRequirements[ScopingType.BaselineBodIndicators][ScopingType.Effects].AlertTypeMissingData = AlertType.Notification;
+            _actionDataLinkRequirements[ScopingType.BaselineBodIndicators][ScopingType.Populations].AlertTypeMissingData = AlertType.Notification;
         }
 
         protected override void loadData(ActionData data, SubsetManager subsetManager, CompositeProgressState progressState) {
