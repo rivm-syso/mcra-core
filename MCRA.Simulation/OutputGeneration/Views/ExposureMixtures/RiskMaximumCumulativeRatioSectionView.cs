bc3c7af9b666a023ecadf620489b7dd8ce2ee86a
Waldo de Boer <waldo.deboer@wur.nl>
2026-03-04 12:00:17 +0100

    docs: add documentation

--- MCRA.Simulation/OutputGeneration/Views/ExposureMixtures/RiskMaximumCumulativeRatioSectionView.cs
+++ MCRA.Simulation/OutputGeneration/Views/ExposureMixtures/RiskMaximumCumulativeRatioSectionView.cs
@@ -17,25 +17,25 @@ namespace MCRA.Simulation.OutputGeneration.Views {
                 : "Risk is defined as hazard/exposure (H/E).";
 
             sb.AppendDescriptionParagraph($"Maximum Cumulative Ratio (MCR) plot: total risk / maximum risk vs total risk (n = {Model.DriverSubstanceTargets.Count}). {riskMetricType}");
-            sb.AppendDescriptionParagraph($"For each {individualDayUnit} the MCR is calculated as the sum of risk characterisation ratios and " +
+            sb.AppendDescriptionParagraph($"Each dot represents the MCR of an {individualDayUnit} and is calculated as the sum of risk characterisation ratios " +
                 $"divided by the ratio of the highest contributing substance.");
             if (Model.RiskMetricType == RiskMetricType.ExposureHazardRatio) {
-                sb.AppendDescriptionParagraph($"The blue area, where E/H < threshold, marks combined exposures that do not present any concerns.");
-                sb.AppendDescriptionParagraph($"The white area defined by the vertical red line for acceptable risks and the curved line " +
-                    $" depicting E/H = threshold * MCR (with default value threshold = 1) shows subjects with combined exposures above the threshold (E/H > threshold) " +
-                    $" but without exceeding the risk quotient for any single substance.");
-                sb.AppendDescriptionParagraph($"The red area is for subjects where E/H > threshold and with exposures producing risk " +
+                sb.AppendDescriptionParagraph($"The blue area, where E/H < threshold, marks combined exposures that do not present any concern.");
+                sb.AppendDescriptionParagraph($"The white area - defined by the vertical red line for acceptable risks and the curved line " +
+                    $" depicting E/H = threshold * MCR (with default threshold of 1) - shows subjects with combined exposures above the threshold (E/H > threshold) " +
+                    $" but whose risk quotient for any single substance does not exceed the limit.");
+                sb.AppendDescriptionParagraph($"The red area represents subjects where E/H > threshold and where exposures produces risk " +
                     $"quotients exceeding the threshold for at least one substance.");
             } else {
-                sb.AppendDescriptionParagraph($"The blue area, where H/E > threshold, marks combined exposures that do not present any concerns.");
-                sb.AppendDescriptionParagraph($"The white area defined by the vertical red line for acceptable risks and the curved line " +
-                    $" depicting H/E = threshold/MCR (with default value threshold = 1) shows subjects with combined exposures below the threshold (H/E < threshold) " +
-                    $" but without exceeding the risk quotient for any single substance.");
-                sb.AppendDescriptionParagraph($"The red area is for subjects where H/E < threshold/MCR and with exposures producing risk " +
+                sb.AppendDescriptionParagraph($"The blue area, where H/E > threshold, marks combined exposures that do not present any concern.");
+                sb.AppendDescriptionParagraph($"The white area defined - by the vertical red line for acceptable risks and the curved line " +
+                    $" depicting H/E = threshold/MCR (with default threshold of 1) - shows subjects with combined exposures below the threshold (H/E < threshold) " +
+                    $" but  whose risk quotient for any single substance does not exceed the limit.");
+                sb.AppendDescriptionParagraph($"The red area represents subjects where H/E < threshold/MCR and where exposures produces risk " +
                     $"quotients exceeding the threshold for at least one substance.");
             }
-            sb.AppendDescriptionParagraph($"Data points below the horizontal red line corresponding to MCR = 2 show subjects in whom one substance " +
-                $"contributed 50% ore more to the risk (e.g. single substance issue). Above the line, subjects experience combined exposures from multiple " +
+            sb.AppendDescriptionParagraph($"Data points below the horizontal red line (corresponding to MCR = 2) indicate subjects where one substance " +
+                $"contributes 50% ore more to the total risk (e.g. single substance issue). Above the line, subjects experience combined exposures from multiple " +
                 $"substances (e.g. co-exposure).");
 
             if (Model.DriverSubstanceTargets.Count > 1) {
